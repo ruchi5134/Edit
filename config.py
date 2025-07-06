bffaecdb6b149b9a58abaa080189e9d983c4a06b
ruchi5134 <kn043613@gmail.com>
2025-07-06 13:47:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '8165978531:AAFqna4SR97th3Ll-NoFvTZ0dA8zwzUTsAI' # Get Your Telegram Bot Token from @BotFather
+TOKEN = '8094680258:AAH0gDa8RQLdK6Aw9G0PeXZGgJa4jtF90LE' # Get Your Telegram Bot Token from @BotFather
 OWNER_ID = '5758713974' # Get Your Owner Id From @Obanai_XRobot
